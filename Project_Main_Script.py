ddbd7d7d5a8ae0165cb2272a507dd3c91367ad38
jpkretschmann <119436991+jpkretschmann@users.noreply.github.com>
2022-12-13 17:04:31 +0100

    Update Project_Main_Script.py

--- Project_Main_Script.py
+++ Project_Main_Script.py
@@ -198,9 +198,21 @@ comorbidity = st.selectbox(
 
 #histogram_comorbidity = px.histogram(df,x=comorbidity, color='Dead',barmode='group', title = 'Distribution of the morbidity in function of '+comorbidity)
 
-histogram_comorbidity = px.histogram(df, x=comorbidity, color= 'Dead', barnorm = 'fraction', barmode='relative', title = 'Distribution of the morbidity in function of '+comorbidity , color_discrete_sequence=px.colors.qualitative.T10, text_auto = True)
-histogram_comorbidity.update_xaxes(type='category')
-st.plotly_chart(histogram_comorbidity)
+if comorbidity == 'Age':
+  histogram_comorbidity = px.histogram(df,x=comorbidity, color='Dead',barmode='group', title = 'Distribution of the morbidity in function of '+cause)
+  st.plotly_chart(histogram_distribution)
+else:
+ histogram_comorbidity = px.histogram(df, x=comorbidity, color= 'Dead', barnorm = 'fraction', barmode='relative', title = 'Distribution of the morbidity in function of '+comorbidity , color_discrete_sequence=px.colors.qualitative.T10, text_auto = True)
+ histogram_comorbidity.update_xaxes(type='category')
+ st.plotly_chart(histogram_comorbidity)
+
+
+
+
+
+#histogram_comorbidity = px.histogram(df, x=comorbidity, color= 'Dead', barnorm = 'fraction', barmode='relative', title = 'Distribution of the morbidity in function of '+comorbidity , color_discrete_sequence=px.colors.qualitative.T10, text_auto = True)
+#histogram_comorbidity.update_xaxes(type='category')
+#st.plotly_chart(histogram_comorbidity)
 #cause_distribution = px.histogram(df,cause, color='Sex', barmode='group', histnorm='percent', title= 'Distribution of '+cause+' among the patients')
 
 

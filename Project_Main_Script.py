ee24c62334a590bb41a0b9c05caec9502697c40d
jpkretschmann <119436991+jpkretschmann@users.noreply.github.com>
2022-12-14 17:33:35 +0100

    Update Project_Main_Script.py

--- Project_Main_Script.py
+++ Project_Main_Script.py
@@ -405,21 +405,6 @@ result1["actual"] = Y_test
 confusion_matrix = confusion_matrix(Y_test, y_pred)
 
 
-y_pred_proba = np.array(df23['y_pred_proba'])
-probit_roc_auc = roc_auc_score(Y_test, y_pred)
-fpr, tpr, thresholds = roc_curve(Y_test, y_pred_proba)
-plt.figure()
-plt.plot(fpr, tpr, label='Probit Model (area = %0.2f)' % probit_roc_auc)
-plt.plot([0, 1], [0, 1],'r--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic')
-plt.legend(loc="lower right")
-plt.savefig('Probit_ROC')
-
-
 
 
 
@@ -436,7 +421,7 @@ with st.expander('Do you want to see behind the scenes of this prediction ? Watc
     st.write('This is of course subject of discussion if that threshold is properly choosen, yet experimentation showed that this threshold yield the best accuracy')
     st.write('Herby we display some metrics to evaluate our model:')
     st.write(result_full.summary())
-    st.write('Accuracy of the Probit Model on test set: {:.2f}'.format(accuracy_score(Y_test, y_pred)))
+    st.write('The accuracy of the Probit Model on test set: {:.2f}'.format(accuracy_score(Y_test, y_pred)))
     st.write(confusion_matrix)
     st.write(classification_report(Y_test, y_pred))
-    st.write(plt.show())
+    #st.write(plt.show())
